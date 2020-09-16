d15ba9c2bf67a61c618233cc2c808231c5918bce
jhjang92 <jhjang92@kakao.com>
2020-09-17 01:05:33 +0900

    mission 01 완성

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,17 @@ import React from 'react';
 
 class Button extends React.Component {
   render() {
-    return (
-      <>
-        <button>{this.props.Text}</button>
-      </>
-    )
+    const buttonStyle = {
+      background: this.props.FontColor,
+    };
+    if (this.props.FontColor && this.props.onClick) {
+      return (
+        <button style={buttonStyle} onClick={this.props.onClick}>
+          {this.props.Text}
+        </button>
+      );
+    }
+    return <button>{this.props.Text}</button>;
   }
 }
 
